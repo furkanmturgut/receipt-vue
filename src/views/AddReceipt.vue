8a43117e7c3ba4aa5c78203e9ea5b5f0f3cb5476
furkandlkdr <115067547+furkandlkdr@users.noreply.github.com>
2023-09-11 16:12:45 +0300

    Feature: camera choose

--- src/views/AddReceipt.vue
+++ src/views/AddReceipt.vue
@@ -4,72 +4,151 @@
     <form @submit.prevent="submitData">
       <div id="fatura">
         <h1 style="text-align: center">Fatura</h1>
-        <video ref="videoElement1" class="videoCam" style="display: none" id="video1" autoplay></video>
-        <TfButtonView v-if="!isCameraOn1 || isPhotoTaken1" class="openCamera1" @click="requestCameraAccess1" style="
+        <video
+          ref="videoElement1"
+          class="videoCam"
+          style="display: none"
+          id="video1"
+          autoplay
+        ></video>
+        <TfButtonView
+          v-if="!isCameraOn1 || isPhotoTaken1"
+          class="openCamera1"
+          @click="requestCameraAccess1"
+          style="
             display: flex;
             justify-content: center;
             align-items: center;
             text-align: center;
-          ">Kamerayı aç</TfButtonView>
-        <TfButtonView v-else @click="takePhoto1" style="
+          "
+          >Kamerayı aç</TfButtonView
+        >
+        <TfButtonView
+          v-else
+          @click="takePhoto1"
+          style="
             display: flex;
             justify-content: center;
             align-items: center;
             text-align: center;
-          ">📸</TfButtonView>
+          "
+          >📸</TfButtonView
+        >
+        <!-- Kamera seç  -->
+        <TfDropdownView
+          v-if="!(!isCameraOn1 || isPhotoTaken1)"
+          v-model="currentCamera"
+          :options="availableCameras"
+          optionLabel="label"
+        ></TfDropdownView>
         <canvas ref="canvasElement1" style="display: none"></canvas>
-        <img v-if="isPhotoTaken1" :src="photoScreen1" id="video" alt="Çekilen Fotoğraf" class="videoCam" />
+        <img
+          v-if="isPhotoTaken1"
+          :src="photoScreen1"
+          id="video"
+          alt="Çekilen Fotoğraf"
+          class="videoCam"
+        />
       </div>
       <!-- FATURA/SLİP -->
       <!-- FATURA/SLİP -->
       <!-- FATURA/SLİP -->
       <div class="slip">
         <h1 style="text-align: center">Slip</h1>
-        <video class="videoCam" id="video2" ref="videoElement2" style="display: none" autoplay />
-        <TfButtonView v-if="!isCameraOn2 || isPhotoTaken2" class="openCamera2" @click="requestCameraAccess2" style="
+        <video
+          class="videoCam"
+          id="video2"
+          ref="videoElement2"
+          style="display: none"
+          autoplay
+        />
+        <TfButtonView
+          v-if="!isCameraOn2 || isPhotoTaken2"
+          class="openCamera2"
+          @click="requestCameraAccess2"
+          style="
             display: flex;
             justify-content: center;
             align-items: center;
             text-align: center;
-          ">Kamerayı aç</TfButtonView>
-        <TfButtonView v-else @click="takePhoto2" style="
+          "
+          >Kamerayı aç</TfButtonView
+        >
+        <TfButtonView
+          v-else
+          @click="takePhoto2"
+          style="
             display: flex;
             justify-content: center;
             align-items: center;
             text-align: center;
-          ">📸</TfButtonView>
+          "
+          >📸</TfButtonView
+        >
         <canvas ref="canvasElement2" style="display: none"></canvas>
-        <img v-if="isPhotoTaken2" :src="photoScreen2" class="videoCam" id="video" alt="Çekilen Fotoğraf" />
+        <img
+          v-if="isPhotoTaken2"
+          :src="photoScreen2"
+          class="videoCam"
+          id="video"
+          alt="Çekilen Fotoğraf"
+        />
       </div>
       <!-- SLİP/TARİH -->
       <!-- SLİP/TARİH -->
       <!-- SLİP/TARİH -->
       <h1 style="text-align: center">Fatura Tarihi</h1>
-      <TfInputView :class="{ 'p-invalid': !dateInput }" type="date" v-model="dateInput"
-        style="width: auto; height: 2rem" />
+      <TfInputView
+        :class="{ 'p-invalid': !dateInput }"
+        type="date"
+        v-model="dateInput"
+        style="width: auto; height: 2rem"
+      />
       <!-- TARİH/TUTAR -->
       <!-- TARİH/TUTAR -->
       <!-- TARİH/TUTAR -->
       <h1 style="text-align: center">Tutar</h1>
 
-      <TfInputView :class="{ 'p-invalid': !paymentPrice }" type="number" style="width: auto; height: 2rem"
-        v-model="paymentPrice" placeholder="Ödeme Tutarı (TL)" />
+      <TfInputView
+        :class="{ 'p-invalid': !paymentPrice }"
+        type="number"
+        style="width: auto; height: 2rem"
+        v-model="paymentPrice"
+        placeholder="Ödeme Tutarı (TL)"
+      />
 
       <!-- TUTAR/ÖDEME ŞEKLİ -->
       <!-- TUTAR/ÖDEME ŞEKLİ -->
       <h1 style="text-align: center">Ödeme Şekli</h1>
-      <div style="flex-wrap: wrap; justify-content: center; column-gap: 25px" class="flex">
+      <div
+        style="flex-wrap: wrap; justify-content: center; column-gap: 25px"
+        class="flex"
+      >
         <div class="flex align-items-center">
           <TfRadioView v-model="paymentMethod" name="kart" value="kart" />
-          <label for="kart" style="font-size: 1.5rem; font-weight: bold" class="ml-2">Kart</label>
+          <label
+            for="kart"
+            style="font-size: 1.5rem; font-weight: bold"
+            class="ml-2"
+            >Kart</label
+          >
         </div>
         <div class="flex align-items-center">
           <TfRadioView v-model="paymentMethod" name="nakit" value="nakit" />
-          <label for="nakit" style="font-size: 1.5rem; font-weight: bold" class="ml-2">Nakit</label>
+          <label
+            for="nakit"
+            style="font-size: 1.5rem; font-weight: bold"
+            class="ml-2"
+            >Nakit</label
+          >
         </div>
       </div>
       <!-- OdemeSekli//Kaydet Buton -->
-      <TfButtonView style="text-transform: uppercase; font-size: large" type="submit" label="Kaydet" />
+      <TfButtonView
+        style="text-transform: uppercase; font-size: large"
+        type="submit"
+        label="Kaydet"
+      />
     </form>
   </div>
 </template>
@@ -97,6 +176,7 @@ export default {
   setup() {
     const auth = getAuth();
     const db = getFirestore();
+    const router = useRouter();
     const storage = getStorage();
     const videoElement1 = ref(null);
     const videoElement2 = ref(null);
@@ -113,18 +193,25 @@ export default {
     const isPhotoTaken1 = ref(false);
     const isPhotoTaken2 = ref(false);
     const paymentMethod = ref(null);
-    const router = useRouter();
+    const currentCamera = ref(null);
     let cameraStream1 = null;
     let cameraStream2 = null;
+    const availableCameras = ref([]);
     const newArrayList = ref([]);
 
-
+    const getAvailableCameras = async () => {
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const cameraList = devices.filter((d) => d.kind == "videoinput");
+      availableCameras.value = cameraList;
+      console.log("devicelist:",typeof(cameraList), cameraList);
+    };
 
     const requestCameraAccess1 = async () => {
       try {
         isPhotoTaken1.value = false;
+        getAvailableCameras();
         cameraStream1 = await navigator.mediaDevices.getUserMedia({
-          video: true,
+          video: { deviceId: currentCamera }, //Buraya selectedDevice gelecek
         });
         videoElement1.value.srcObject = cameraStream1;
         videoElement1.value.style.display = "block";
@@ -230,7 +317,6 @@ export default {
         dateInput.value &&
         paymentPrice.value &&
         paymentMethod.value
-
       ) {
         try {
           const id = Date.now();
@@ -244,16 +330,15 @@ export default {
           stopCamera1();
           stopCamera2();
 
-          const cN = query(collection(db, "companyInfo"), where("id", "==", auth.currentUser.uid));
+          const cN = query(
+            collection(db, "companyInfo"),
+            where("id", "==", auth.currentUser.uid)
+          );
           const querySnapshot1 = await getDocs(cN);
           querySnapshot1.forEach((doc) => {
             newArrayList.value.push(doc.data());
             //console.log(newArrayList)
-
           });
-
-
-
 
           addDoc(collection(db, "infos"), {
             id: auth.currentUser.uid,
@@ -286,6 +371,7 @@ export default {
       takePhoto2,
       stopCamera1,
       stopCamera2,
+      getAvailableCameras,
       //
       videoElement1,
       videoElement2,
@@ -297,6 +383,8 @@ export default {
       photoPng2,
       dateInput,
       paymentMethod,
+      availableCameras,
+      currentCamera,
       paymentPrice,
       //
       isCameraOn1,
